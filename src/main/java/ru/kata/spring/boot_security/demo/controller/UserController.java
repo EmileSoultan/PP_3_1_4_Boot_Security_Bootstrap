bf89fe37ee3e6ba707a4c0c02b0dde981c9b9b51
Emile <emabish42@gmail.com>
2023-05-20 09:23:25 +0200

    Контроллер для отображения представления(view) для обычного польз-ля

---
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/UserController.java
@@ -0,0 +1,29 @@
+package ru.kata.spring.boot_security.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ru.kata.spring.boot_security.demo.repository.RoleRepository;
+import ru.kata.spring.boot_security.demo.service.UserService;
+
+import java.security.Principal;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+    private final UserService userService;
+    private final RoleRepository roleRepository;
+
+    public UserController(UserService userService, RoleRepository roleRepository) {
+        this.userService = userService;
+        this.roleRepository = roleRepository;
+    }
+
+    @GetMapping()
+    public String userPage(ModelMap model, Principal principal) {
+        long id = userService.findByEmail(principal.getName()).orElseThrow().getId();
+        model.addAttribute("user", userService.findById(id));
+        return "user";
+    }
+}
