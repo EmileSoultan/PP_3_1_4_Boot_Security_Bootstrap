d3aaf5593c69dc5fc5a27124753c4f6e869898a8
Emile <emabish42@gmail.com>
2023-05-20 08:54:09 +0200

    Initial commit

---
+++ src/main/java/ru/kata/springboot_security_bootstrapdemo/SpringBootSecurityBootstrapDemoApplication.java
@@ -0,0 +1,13 @@
+package ru.kata.springboot_security_bootstrapdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecurityBootstrapDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSecurityBootstrapDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/kata/springboot_security_bootstrapdemo/SpringBootSecurityBootstrapDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kata.springboot_security_bootstrapdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSecurityBootstrapDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
